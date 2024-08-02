e7afa224f3de299663cc04c6785f26c8fa93e85b
Manasa B Rao <manasabrao2020@gmail.com>
2024-08-02 08:19:30 +0530

    updated

---
+++ tele_echo_bot.py
@@ -0,0 +1,37 @@
+import logging
+from aiogram import Bot, Dispatcher,executor,types
+from dotenv import load_dotenv
+import os
+
+
+
+load_dotenv()
+API_TOKEN = os.getenv('TOKEN')
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` and '/help' command
+    """
+    
+    await message.reply("Hi \nI am Echo Bot\n Powered by aiogram")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    """
+    This will return echo
+    """
+    
+    await message.answer(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp,skip_updates=True)
+
+
